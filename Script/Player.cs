fb776906744653bcb70d156607a352df5bf0a33c
NaughtyFishRei <35935912+NaughtyFishRei@users.noreply.github.com>
2018-12-19 01:33:09 +0000

    181219-0132
    移动，延迟移动，开头不移动

--- Script/Player.cs
+++ Script/Player.cs
@@ -4,42 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float moveSpeed = 0.1f;
+    private PlayerMovement moveScript;
 
-    [SerializeField] private char nextMoveDir;
-
-    void Start() {
-        //enabled = false;
-    }
-
-    private void Update() {
-        if (Input.GetKeyDown("w")) {
-            nextMoveDir = 'w';
-        }else if (Input.GetKeyDown("a")) {
-            nextMoveDir = 'a';
-        } else if (Input.GetKeyDown("s")) {
-            nextMoveDir = 's';
-        } else if (Input.GetKeyDown("d")) {
-            nextMoveDir = 'd';
+	void Start () {
+        moveScript = GetComponent<PlayerMovement>();
+        moveScript.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moveScript.enabled == false) {
+            if (Input.GetKeyDown("w")) {
+                moveScript.setNextMoveDir('w');
+                moveScript.enabled = true;
+            } else if (Input.GetKeyDown("a")) {
+                moveScript.setNextMoveDir('a');
+                moveScript.enabled = true;
+            } else if (Input.GetKeyDown("s")) {
+                moveScript.setNextMoveDir('s');
+                moveScript.enabled = true;
+            } else if (Input.GetKeyDown("d")) {
+                moveScript.setNextMoveDir('d');
+                moveScript.enabled = true;
+            }
         }
-
-        if(nextMoveDir == 'a') {
-            transform.localEulerAngles = new Vector3(0, -90, 0);
-        }else if(nextMoveDir == 's') {
-            transform.localEulerAngles = new Vector3(0, 180, 0);
-        } else if (nextMoveDir == 'd') {
-            transform.localEulerAngles = new Vector3(0, 90, 0);
-        } else if (nextMoveDir == 'w') {
-            transform.localEulerAngles = new Vector3(0, 0, 0);
-        }
-
-        RaycastHit hit1;
-        bool hitornot = Physics.Linecast(gameObject.transform.position + new Vector3(0.5f, 0, 0), gameObject.transform.position + new Vector3(0.5f, 0, 0.5f));
-        Debug.DrawLine(gameObject.transform.position + new Vector3(0.5f, 0, 0), gameObject.transform.position + new Vector3(0.5f, 0, 0.5f));
-        Debug.Log(hitornot);
-    }
-
-    void FixedUpdate() {
-        transform.Translate(Vector3.forward * moveSpeed);
-    }
+	}
 }
---
+++ Script/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 0.1f;
+
+    [SerializeField] private char nextMoveDir;
+
+    void Start() {
+        //enabled = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("w")) {
+            nextMoveDir = 'w';
+        } else if (Input.GetKeyDown("a")) {
+            nextMoveDir = 'a';
+        } else if (Input.GetKeyDown("s")) {
+            nextMoveDir = 's';
+        } else if (Input.GetKeyDown("d")) {
+            nextMoveDir = 'd';
+        }
+
+        if (nextMoveDir == 'a' && Valid('a')) {
+            transform.localEulerAngles = new Vector3(0, -90, 0);
+        } else if (nextMoveDir == 's' && Valid('s')) {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        } else if (nextMoveDir == 'd' && Valid('d')) {
+            transform.localEulerAngles = new Vector3(0, 90, 0);
+        } else if (nextMoveDir == 'w' && Valid('w')) {
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    void FixedUpdate() {
+        transform.Translate(Vector3.forward * moveSpeed);
+    }
+
+    bool Valid(char dir) {
+        RaycastHit hit;
+        Vector3 dirVector = new Vector3(0, 0, 0);
+        if (dir == 'a') {
+            dirVector = new Vector3(-1, 0, 0);
+        } else if (dir == 's') {
+            dirVector = new Vector3(0, 0, -1);
+        } else if (dir == 'd') {
+            dirVector = new Vector3(1, 0, 0);
+        } else if (dir == 'w') {
+            dirVector = new Vector3(0, 0, 1);
+        }
+        Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + dirVector, Color.blue);
+        if (Physics.Linecast(gameObject.transform.position, gameObject.transform.position + dirVector, out hit)) {
+            return false;       //在想要走向的方向上有障碍，无效
+        } else {
+            return true;        //无障碍，有效
+        }
+    }
+
+    public char getNextMoveDir() {
+        return nextMoveDir;
+    }
+
+    public void setNextMoveDir(char dir) {
+        nextMoveDir = dir;
+    }
+}
