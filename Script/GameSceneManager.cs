66144688104eb4eebbf8e335fb710e659d600639
NaughtyFishRei <35935912+NaughtyFishRei@users.noreply.github.com>
2019-02-04 00:23:36 +0000

    0204-0023
    添加调试UI

--- Script/GameSceneManager.cs
+++ Script/GameSceneManager.cs
@@ -10,7 +10,8 @@ public class GameSceneManager : MonoBehaviour {
     public Transform startPoint;
     public int mazeWidth;           //长宽包含了边界的2格,一定是奇数
     public int mazeHeight;
-    public Button btn;
+    public Button removeBtn;
+    public Button buildMazeBtn;
 
     [SerializeField]private GameObject maze;
     [SerializeField]private GameObject pacMan;
@@ -23,25 +24,36 @@ public class GameSceneManager : MonoBehaviour {
     void Start () {
         pacMan = this.GetComponent<PacManRespawn>().respawnPacMan(startPoint);
         virtualCamera1.Follow = pacMan.transform;
-        if (Prim) {
-            mazeGenerator = GetComponent<MazeGenPrim>();
-            maze = mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0] != null);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2] != null);
-        } else if (RD) {
-            mazeGenerator = GetComponent<MazeGenRD>();
-            maze = mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0] != null);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2] != null);
-        }else if (RB) {
-            mazeGenerator = GetComponent<MazeGenRB>();
-            maze = mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0] != null);
-            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2] != null);
-        }
-        btn.onClick.AddListener(delegate () {mazeGenerator.RemoveDeadEnds(mazeWidth,mazeHeight);});
+        BuildMaze();
+        removeBtn.onClick.AddListener(delegate () {mazeGenerator.RemoveDeadEnds(mazeWidth,mazeHeight);});
+        buildMazeBtn.onClick.AddListener(delegate () { this.BuildMaze(); });
     }
 
     void Update () {
+    }
+
+    public void BuildMaze() {
+        if(maze !=null) {
+            Destroy(maze);
+        }
+        if (Prim) {
+            mazeGenerator=GetComponent<MazeGenPrim>();
+            maze=mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0]!=null);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2]!=null);
+            Debug.Log("Maze is built by Prim");
+        } else if (RD) {
+            mazeGenerator=GetComponent<MazeGenRD>();
+            maze=mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0]!=null);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2]!=null);
+            Debug.Log("Maze is built by Prim");
+        } else if (RB) {
+            mazeGenerator=GetComponent<MazeGenRB>();
+            maze=mazeGenerator.GenerateMaze(mazeWidth, mazeHeight);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[0, 0]!=null);
+            Debug.Log(maze.GetComponent<Maze>().mazeObjects[1, 2]!=null);
+            Debug.Log("Maze is built by Prim");
+        }
     }
 }
