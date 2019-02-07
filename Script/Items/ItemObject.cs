37d3e2b735defee6a2fd8c06b3742dd5521e5c67
NaughtyFishRei <35935912+NaughtyFishRei@users.noreply.github.com>
2019-02-07 00:37:36 +0000

    0207-0037
    itemobject模型

--- Script/Items/ItemObject.cs
+++ Script/Items/ItemObject.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ItemObject : MonoBehaviour {
 
     public string itmeName;
+    public float rotateSpeed = 1;
 
+    private void FixedUpdate() {
+        this.transform.Rotate(new Vector3(0,2,0)*rotateSpeed);
+    }
     //protected abstract void TriggeredByPlayer(GameObject player);
 
     /*private void OnTriggerEnter(Collider other) {
